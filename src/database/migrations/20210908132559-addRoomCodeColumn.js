54d1fe7dfd44cd1a97c747d395d61df80297df4a
Bruno Lages <bruno.bc13513@gmail.com>
2021-09-08 17:25:14 -0300

    update code allow null again

--- src/database/migrations/20210908132559-addRoomCodeColumn.js
+++ src/database/migrations/20210908132559-addRoomCodeColumn.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.addColumn('rooms', 'code', { type: Sequelize.STRING, allowNull: false });
+        await queryInterface.addColumn('rooms', 'code', { type: Sequelize.STRING, allowNull: true });
     },
     
     down: async (queryInterface, Sequelize) => {
